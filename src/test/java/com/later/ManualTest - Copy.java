7d09d3be9db2d8df31334527b5053c07b9ac9ee6
RakeshNagaraj7848 <rakeshpawar1717@gmail.com>
2024-10-05 11:30:04 +0530

    second commit

---
+++ src/test/java/com/later/ManualTest - Copy.java
@@ -0,0 +1,12 @@
+package com.later;
+
+public class ManualTest {
+	public static void main(String[] args) {
+		//ChromeDriver driver = new ChromeDriver();
+		//added pull line
+
+
+// added push line
+	}
+
+}
